bd848d21890b8690e30e4bf8e69b7032ade6a608
shotaro ogawa <ogawashotaro@gmail.com>
2024-04-06 16:03:25 +0900

    testCode  modified add successTest

--- __test__/models/Todo/create.spec.js
+++ __test__/models/Todo/create.spec.js
@@ -24,4 +24,19 @@ describe("createメソッドのテスト", () => {
       }
     });
   });
+  it("メソッド実行時、正しい引数を渡すと、新規にTodoデータを作成し、Todo配列にデータを追加する", () => {
+    const data = {
+      title: "ダミータイトル",
+      body: "ダミーボディ",
+    };
+
+    const createTodo = Todo.create(data);
+    expect(createTodo).toEqual({
+      id: createTodo.id,
+      title: data.title,
+      body: data.body,
+      createdAt: createTodo.createdAt,
+      updatedAt: createTodo.updatedAt,
+    });
+  });
 });
